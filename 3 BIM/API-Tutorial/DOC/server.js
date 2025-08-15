7e38226387be99b55c858faa10749981252f5c79
Lucas Tedesco <tedescosantoslucas955@gmail.com>
2025-08-15 09:56:17 -0300

    Commit

--- 3 BIM/API-Tutorial/DOC/server.js
+++ 3 BIM/API-Tutorial/DOC/server.js
@@ -1,6 +1,10 @@
 import express from "express";
+import path from 'path';
+import {fileURLToPath} from 'url';
 import dataRoutes from "./routers/routes.js";
 
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +14,15 @@ app.use(express.json());
 
 //Rota da API 
 app.use('/', dataRoutes)
+
+
+app.use(express.static(path.join(_dirname, 'public')))
+
+
+//Rota inicial
+app.get('/', (req, res) => {
+    res.sendFile(path.join(_dirname, 'public', 'index.html'))
+});
 
 app.listen(PORT, () => {
     console.log("Servidor rodando em localhost:3000")
